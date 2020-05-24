79d8a5d3a7ede720d5035a4fb5a5c84859f371a4
Khoa Tran <tdkhoa.angiang@gmail.com>
2020-05-24 13:32:56 +0000

    [develop] - [CHANGED] seeder

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Admin;
 
 class AdminSeeder extends Seeder
 {
@@ -11,6 +12,40 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Models\Admin::class, 10)->create();
+        Admin::create([
+            'name'              => 'Trần Đăng Khoa',
+            'email'             => 'tdkhoa.angiang@gmail.com',
+            'gender'            => 1,
+            'avatar'            => 'no-avatar.png',
+            'birthday'          => '1993-04-16',
+            'email_verified_at' => now(),
+            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token'    => Str::random(10),
+            'img_mtime' => now(),
+        ]);
+
+        Admin::create([
+            'name'              => 'Huỳnh Quốc Tấn',
+            'email'             => 'tdkhoa.angiang@gmail.com',
+            'gender'            => 1,
+            'avatar'            => 'no-avatar.png',
+            'birthday'          => '1995-01-01',
+            'email_verified_at' => now(),
+            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token'    => Str::random(10),
+            'img_mtime' => now(),
+        ]);
+
+        Admin::create([
+            'name'              => 'Võ Thanh Hòa',
+            'email'             => 'tdkhoa.angiang@gmail.com',
+            'gender'            => 1,
+            'avatar'            => 'no-avatar.png',
+            'birthday'          => '1995-01-01',
+            'email_verified_at' => now(),
+            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token'    => Str::random(10),
+            'img_mtime' => now(),
+        ]);
     }
 }
